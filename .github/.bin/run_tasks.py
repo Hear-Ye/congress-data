fc89eb585a35d2546d2daed8bd16f135eb21e560
Andrew Chen Wang <60190294+Andrew-Chen-Wang@users.noreply.github.com>
2021-08-18 00:25:42 -0400

    Restore files if only "updated" changed (Fixes #1)

--- .github/.bin/run_tasks.py
+++ .github/.bin/run_tasks.py
@@ -1,16 +1,17 @@
 import glob
 import shutil
+import subprocess
 from datetime import datetime
 from math import floor
 from pathlib import Path
 from uuid import uuid4
 
-from congress_crawler.votes import run as run_votes
+from congress_crawler.votes import run as _run_votes
 
 ROOT_DIR = Path(__file__).parent.parent.parent
 
 
-def organize_files_to_temp_dir(data_type):
+def archive_data(data_type: str):
     temp_dir = ROOT_DIR / str(uuid4())
     temp_dir.mkdir(exist_ok=False)
     for _dir in glob.glob(f"{ROOT_DIR / 'data'}/*/{data_type}/*"):
@@ -19,7 +20,7 @@ def organize_files_to_temp_dir(data_type):
     shutil.rmtree(temp_dir)
 
 
-def _run_votes():
+def run_votes():
     # To get beginning to now:
     # run_votes(dict(congress="101", session="1989", chamber="senate"))
     # run_votes(dict(congress="101", session="1990"))
@@ -28,13 +29,30 @@ def _run_votes():
 
     # Gets the latest votes along with updated votes for our entire year
     latest = datetime.now().year - 1991
-    run_votes(dict(congress=str(floor(102 + latest / 2)), session=str(1991 + latest)))
+    _run_votes(dict(congress=str(floor(102 + latest / 2)), session=str(1991 + latest)))
+    prior = latest - 1
+    _run_votes(dict(congress=str(floor(102 + prior / 2)), session=str(1991 + prior)))
+    
+    # Restore modified files that only changed the updated_at field
+    # git diff does not include new files
+    git_diff = subprocess.check_output(["git", "diff", "--stat"]).decode("utf-8")
+    all_unnecessary_paths = [
+        # Gets that path
+        x.split("|")[0].strip()
+        # Splits the git diff --stat command
+        for x in git_diff.split("\n")
+        # Checks if it's a path with changes
+        # Then sees if it's 1 addition + 1 subtraction only (just the updated field)
+        if "|" in x and "2 +-" in x.split("|", maxsplit=1)[1]
+    ]
+    subprocess.run(["git", "restore"] + all_unnecessary_paths)
+
     # Congress doesn't pass anything on Jan 1+2 and then Congress
     # swears in new members on the 3rd when the new bills roll in
     # for the new Congress.
-    organize_files_to_temp_dir("votes")
+    archive_data("votes")
 
 
 if __name__ == "__main__":
-    _run_votes()
+    run_votes()
 
